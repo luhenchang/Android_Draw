2988452f4ba312f1e208662246251832615b7b81
wangfei44 <wangfei44@lenovo.com>
2021-06-04 13:36:15 +0800

    [Android_Draw] 录音机快捷菜单提交

---
+++ app/src/main/java/com/example/android_draw/view/RecorderActivity.java
@@ -0,0 +1,13 @@
+package com.example.android_draw.view;
+
+/**
+ * 版权：联想 版权所有
+ *
+ * @author wangfei
+ * 版本：1.5
+ * 创建日期：6/4/21
+ * 描述：Android_Draw
+ */
+public class RecorderActivity {
+    //luhenchang_branch 枝提交
+}
