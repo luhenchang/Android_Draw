490d7db3c71f9f8310c9b2c5c68013e7e935e550
wangfei44 <wangfei44@lenovo.com>
2021-06-08 21:14:08 +0800

    [luhenchang_branch]合并分支代码到主分支上

--- app/src/main/java/com/example/android_draw/view/RecorderActivity.java
+++ app/src/main/java/com/example/android_draw/view/RecorderActivity.java
@@ -11,4 +11,7 @@ package com.example.android_draw.view;
 public class RecorderActivity {
     //luhenchang_branch 枝提交
     //master 第一次修改 需要 合并到 luhenchang_branch
+    //master 第二次修改 需要 合并到 luhenchang_branch
+    //master 第三次修改 需要 合并到 luhenchang_branch
+    //master 第四次修改 需要 合并到 luhenchang_branch
 }
