bf4111192cfbe99114afd1c7fef295c63e94f46b
wangfei44 <wangfei44@lenovo.com>
2021-06-08 21:06:59 +0800

    [Master] 需要合并到luhenchang_branch 03

--- app/src/main/java/com/example/android_draw/view/RecorderActivity.java
+++ app/src/main/java/com/example/android_draw/view/RecorderActivity.java
@@ -12,5 +12,7 @@ public class RecorderActivity {
     //luhenchang_branch 枝提交
     //master 第一次修改 需要 合并到 luhenchang_branch
     //master 第二次修改 需要 合并到 luhenchang_branch
+    //master 第三次修改 需要 合并到 luhenchang_branch
+
 
 }
